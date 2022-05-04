859c430875f121a08e35fca8f417a338fc0fab0e
siddhartha chiuchiolo <siddychiuchiolo7@gmail.com>
2022-05-04 17:10:43 +0200

    inserito cartella img con le immagine e ceato l'header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,37 @@
+<header>
+    <nav class="my-container">
+        <img src="{{ asset('img/dc-logo.png')}}" alt="">
+        <ul>
+            <li>
+                <a href="{{ url('/')}}" >CHARACTERS</a>
+            </li>
+            <li>
+                <a href="{{ url('/')}}" class="active">COMICS</a>
+            </li>
+            <li>
+                <a href="{{ url('/')}}" >MOVIES</a>
+            </li>
+            <li>
+                <a href="{{ url('/')}}" >TV</a>
+            </li>
+            <li>
+                <a href="{{ url('/')}}" >GAMES</a>
+            </li>
+            <li>
+                <a href="{{ url('/')}}" >COLLECTIBLES</a>
+            </li>
+            <li>
+                <a href="{{ url('/')}}" >VIDEOS</a>
+            </li>
+            <li>
+                <a href="{{ url('/')}}" >FANS</a>
+            </li>
+            <li>
+                <a href="{{ url('/')}}" >NEWS</a>
+            </li>
+            <li>
+                <a href="{{ url('/')}}" >SHOP</a>
+            </li>
+        </ul>
+    </nav>
+</header>
